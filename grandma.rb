69dcf68af3572d16be39fb1dc537b8afa7fa633e
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-01-31 20:40:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,7 @@ def speak_to_grandma(string)
   elsif "#{string}" == ("#{string}".upcase)
     return "NO, NOT SINCE 1938!"
 
-  else "#{string}" != ("#{string}".upcase)
+  else 
     return "HUH?! SPEAK UP, SONNY!"
   end
 end
