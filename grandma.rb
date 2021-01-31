4e755d49f5effa5b75db9fc475758f5b666b6e69
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-01-31 20:15:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
-  if string() == "I LOVE YOU GRANDMA!"
+  if string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TO PUMPKIN"
 
-  elsif str() == "".upcase
+  elsif string == ("".upcase)
     return "NO, NOT SINCE 1938!"
 
   else
